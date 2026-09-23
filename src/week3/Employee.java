252ea54220812c29365ee23345ccc1d85e1e3f50
ra0757-cpu <ra0757@srmist.edu.in>
2026-09-23 23:37:02 +0530

    Add Week 3 assignments

---
+++ src/week3/Employee.java
@@ -0,0 +1,17 @@
+package week3;
+
+public class Employee {
+
+    String empName;
+    double salary;
+    boolean permanent;
+
+    public static void main(String[] args) {
+
+        Employee employee = new Employee();
+
+        System.out.println("Name: " + employee.empName);
+        System.out.println("Salary: " + employee.salary);
+        System.out.println("Permanent: " + employee.permanent);
+    }
+}
---
+++ src/week3/Product.java
@@ -0,0 +1,20 @@
+package week3;
+
+public class Product {
+
+    String productId;
+    String productName;
+
+    public Product(String productId, String productName) {
+
+        this.productId = productId;
+        this.productName = productName;
+    }
+
+    public static void main(String[] args) {
+
+        Product product = new Product("P-1042", "Wireless Mouse");
+
+        System.out.println(product.productId + " - " + product.productName);
+    }
+}
